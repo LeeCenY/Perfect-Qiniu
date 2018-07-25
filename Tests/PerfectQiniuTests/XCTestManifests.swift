04e7d264a9f3f53d6505abdfa08e6333bde10a85
LeeCenY <634348197@qq.com>
2018-07-25 18:59:07 +0800

    Annotation test case

--- Tests/PerfectQiniuTests/XCTestManifests.swift
+++ Tests/PerfectQiniuTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(PerfectQiniuTests.allTests),
+//        testCase(PerfectQiniuTests.allTests),
     ]
 }
 #endif
